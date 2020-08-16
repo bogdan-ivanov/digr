78fa2b60ff8563936b1c1715e06937242b2d105e
Bogdan Ivanov <ivanov.george.bogdan@gmail.com>
2020-08-16 01:15:55 +0100

    VirusTotal Scraper

--- scrape.py
+++ scrape.py
@@ -15,23 +15,31 @@ class DomainScraper(object):
     def __init__(self, domain, **kwargs):
         self.domain = domain
         self.kwargs = kwargs
+        self.subdomains = []
 
     async def run(self, results):
         raise NotImplementedError("Implement run method")
 
-    def report_results(self, results, domains):
-        for domain in domains:
+    def report_results(self, results):
+        for domain in self.subdomains:
             if domain not in results:
                 results[domain] = []
 
             results[domain].append(self.__class__.SOURCE)
+
+    def print_table(self):
+        if self.subdomains:
+            click.echo(f"{self.SOURCE} - {self.domain}")
+            click.echo("===============")
+            for domain in self.subdomains:
+                success(f"\t - {domain}")
+            click.echo("\n")
 
 
 class CrtShScraper(DomainScraper):
     SOURCE = 'crt.sh'
 
     async def run(self, results):
-        found_domains = []
         API_URL = f'https://crt.sh/?q=%25.{self.domain}'
         params = dict(
             follow_redirects=True,
@@ -48,19 +56,12 @@ class CrtShScraper(DomainScraper):
             domains = item['domain'].split('<BR>')
             domains = [d for d in domains if d.endswith(self.domain)
                        and '*' not in d and '@' not in d]
-            found_domains.extend(domains)
+            self.subdomains.extend(domains)
 
-        found_domains = set(found_domains)
-        self.report_results(results, found_domains)
+        self.report_results(results)
+        self.print_table()
 
-        if results:
-            click.echo(f"Crt.sh - {self.domain}")
-            click.echo("===============")
-            for domain in results:
-                success(f"Found: {domain}")
-            click.echo("\n")
-
-        return_ = {'source': 'crt.sh', 'results': sorted(list(found_domains))}
+        return_ = {'source': self.SOURCE, 'results': sorted(list(self.subdomains))}
         return return_
 
 
@@ -75,19 +76,12 @@ class SublisterAPIScraper(DomainScraper):
             headers={'User-Agent': random.choice(defaults.USER_AGENTS)}
         )
         response = await asks.get(API_URL, **params)
-        found_domains = sorted(list(set(json.loads(response.content))))
+        self.subdomains = sorted(list(set(json.loads(response.content))))
 
-        self.report_results(results, found_domains)
+        self.report_results(results)
+        self.print_table()
 
-        if results:
-            click.echo(f"api.sublist3r.com - {self.domain}")
-            click.echo("===============")
-            for domain in results:
-                success(f"Found: {domain}")
-
-            click.echo("\n")
-
-        return_ = {'source': 'api.sublist3r.com', 'results': found_domains}
+        return_ = {'source': 'api.sublist3r.com', 'results': self.subdomains}
         return return_
 
 
@@ -108,21 +102,57 @@ class ThreatCrowdScraper(DomainScraper):
             warning(f"ThreatCrowd didn't include any subdomains for {self.domain}")
             found_domains = []
 
-        found_domains = [d.strip() for d in found_domains]
-        found_domains = [d for d in found_domains if d.endswith(self.domain)]
+        self.subdomains = [d.strip() for d in found_domains]
+        self.subdomains = [d for d in found_domains if d.endswith(self.domain)]
 
-        self.report_results(results, found_domains)
+        self.report_results(results)
+        self.print_table()
 
-        if results:
-            click.echo(f"ThreadCrowd.org - {self.domain}")
-            click.echo("===============")
-            for domain in results:
-                success(f"Found: {domain}")
-
-            click.echo("\n")
-
-        return_ = {'source': 'threatcrowd.org', 'results': found_domains}
+        return_ = {'source': 'threatcrowd.org', 'results': self.subdomains}
         return return_
+
+
+class VirusTotalScraper(DomainScraper):
+    SOURCE = 'www.virustotal.com'
+
+    async def run(self, results):
+        API_URL = f'https://www.virustotal.com/ui/domains/{self.domain}/subdomains'
+        params = dict(
+            follow_redirects=True,
+            retries=defaults.DEFAULT_RETRIES,
+            headers={'User-Agent': random.choice(defaults.USER_AGENTS)}
+        )
+
+        while True:
+            response = await asks.get(API_URL, **params)
+            payload = json.loads(response.content)
+            print(json.dumps(payload, indent=2))
+            if 'error' in payload:
+                # print("Error")
+                break
+
+            for item in payload['data']:
+                if item['type'] != 'domain':
+                    continue
+
+                domain = item['id']
+                if not domain.endswith(self.domain) or domain == self.domain:
+                    continue
+
+                self.subdomains.append(domain)
+
+            # print("Found Subdomains: ", found_domains)
+            # input("Press Enter ...")
+
+            if 'links' in payload and 'next' in payload['links']:
+                API_URL = payload['links']['next']
+            else:
+                break
+
+        self.report_results(results)
+        self.print_table()
+
+        return {'source': self.SOURCE, 'results': sorted(list(set(self.subdomains)))}
 
 
 async def scrape_subdomains(domain, scrapers):
@@ -139,4 +169,5 @@ SCRAPERS = [
     CrtShScraper,
     SublisterAPIScraper,
     ThreatCrowdScraper,
+    VirusTotalScraper,
 ]
